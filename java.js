aa66ca20a7ed5cc149292ee503b04929f54d37d7
ibzsayed <120859400+ibzsayed@users.noreply.github.com>
2023-02-23 00:40:44 +0000

    Update java.js
    Added button to the transition in the hamburger menu. Made facebook icon into a button

--- java.js
+++ java.js
@@ -2,4 +2,16 @@ function menuOnClick() {
     document.getElementById("menu-bar").classList.toggle("change");
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
+    document.querySelector('.buttons').classList.toggle('change');
+  }
+
+  function copyEmail() {
+    var email = "Aidan.Boxall@NCCLondon.ac.uk";
+    var tempInput = document.createElement("input");
+    tempInput.value = email;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+    alert("Email address copied to clipboard");
   }
